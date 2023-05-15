1bed5469709120cfaf97aa1e96c7b84219531cf4
Rodrigo Coimbra <rodscoimbra@gmail.com>
2023-05-15 22:56:29 +0100

    Pontinhos qualquer mapa, lento

--- src/montecarlo.py
+++ src/montecarlo.py
@@ -64,10 +64,12 @@ class mcl():
         self.map_sub = rospy.Subscriber('/map', OccupancyGrid, self.map_callback)
         self.position_pub = rospy.Publisher("/particles", MarkerArray, queue_size=1)
         self.timer()
-        self.arrayx = []
-        self.arrayy = []
+        self.arraymap = []
         self.MkArray = MarkerArray()
         self.resolution = 0.0500000
+        self.numero_particulas = 5000
+        self.position_particula=[]
+        self.tamanho_livre = None
 
     def odom_callback(self,msg):
         self.x = msg.pose.pose.position.x 
@@ -77,7 +79,7 @@ class mcl():
         self.v = msg.twist.twist.linear.x
         self.omega = msg.twist.twist.angular.z
         self.t2 = msg.header.stamp.secs + msg.header.stamp.nsecs * (10**-9)
-        if self.t1 != 0:
+        if self.t1 != 0 and self.tamanho_livre != None:
             Particula.prediction(self.particles, self.v, self.omega, self.t2-self.t1)
         self.t1 = self.t2
         
@@ -86,60 +88,62 @@ class mcl():
         self.scan = msg.ranges
 
     def conversao_metros(self,x):
-        return (x-75)*self.resolution
+        return (x)*self.resolution+self.origin_x
+        
     
+    def espaco_livre(self):
+        for x in range(self.width):
+            for y in range(self.height):
+                if(self.map_data[x][y] == 0):
+                    self.arraymap.append([x,y])
+        self.tamanho_livre = len(self.arraymap)
+        x = np.random.randint(self.tamanho_livre, size=self.numero_particulas)
+        for i in x:
+            self.position_particula.append(self.arraymap[i])
+        self.position_particula=np.array(self.position_particula)
+
     def map_callback(self, msg):
         # Convert the 1D array to a 2D numpy array
-        width = msg.info.width
-        height = msg.info.height
+        self.width = msg.info.width
+        self.height = msg.info.height
         self.resolution = msg.info.resolution
         self.origin_x = msg.info.origin.position.x
         self.origin_y = msg.info.origin.position.y
-        self.map_data = np.array(msg.data).reshape((height, width))
-        for x in range(width):
-            for y in range(height):
-                if(self.map_data[x,y] == 100):
-                    self.arrayx.append(x)
-                    self.arrayy.append(y)
-
-        max_x = max(self.arrayx)
-        max_y = max(self.arrayy)
-        min_x = min(self.arrayx)
-        min_y = min(self.arrayy)
-        self.numero_particulas = 500
-
-        self.particles = Particula(np.random.uniform(self.conversao_metros(min_x), self.conversao_metros(max_x), self.numero_particulas), np.random.uniform(self.conversao_metros(min_y), self.conversao_metros(max_y), self.numero_particulas), np.random.uniform(-np.pi, np.pi,self.numero_particulas), self.numero_particulas)
+        self.map_data = np.array(msg.data).reshape((self.height, self.width))
+        self.espaco_livre()
+        self.particles = Particula(self.conversao_metros(self.position_particula[:,1]), self.conversao_metros(self.position_particula[:,0]), np.random.uniform(-np.pi, np.pi,self.numero_particulas), self.numero_particulas)
 
     def timer(self):
         self.timer = rospy.Timer(rospy.Duration(1), self.publish)
         self.h_timerActivate = True
 
     def publish(self, msg):
-        for i in range(len(self.particles.x)):
-            msg = Marker()
-            msg.action = msg.ADD
-            msg.type = msg.SPHERE
-            msg.id = i;
-            msg.pose.position.x = self.particles.x[i]
-            msg.pose.position.y = self.particles.y[i]
-            msg.pose.orientation.z = self.particles.qz[i]
-            msg.pose.orientation.w = self.particles.qw[i]
-            msg.color.a = 1.0
-            msg.color.r = 0.0
-            msg.color.g = 1.0
-            msg.color.b = 0.0
-            #msg.scale.x = 0.25
-            #msg.scale.y = 0.05
-            #msg.scale.z = 0.05
-            msg.scale.x = 0.02
-            msg.scale.y = 0.02
-            msg.scale.z = 0.02
-            msg.header.stamp = rospy.Time.now() 
-            msg.header.frame_id = "base_footprint"
-            if(i>self.numero_particulas):
-                self.MkArray.markers.pop(0)
-            self.MkArray.markers.append(msg)
-        self.position_pub.publish(self.MkArray)   
+        if(self.tamanho_livre != None):    
+            for i in range(len(self.particles.x)):
+                msg = Marker()
+                msg.action = msg.ADD
+                msg.type = msg.SPHERE
+                msg.id = i;
+                msg.pose.position.x = self.particles.x[i]
+                msg.pose.position.y = self.particles.y[i]
+                msg.pose.orientation.z = self.particles.qz[i]
+                msg.pose.orientation.w = self.particles.qw[i]
+                msg.color.a = 1.0
+                msg.color.r = 0.0
+                msg.color.g = 1.0
+                msg.color.b = 0.0
+                #msg.scale.x = 0.25
+                #msg.scale.y = 0.05
+                #msg.scale.z = 0.05
+                msg.scale.x = 0.02
+                msg.scale.y = 0.02
+                msg.scale.z = 0.02
+                msg.header.stamp = rospy.Time.now() 
+                msg.header.frame_id = "base_footprint"
+                if(i>self.numero_particulas):
+                    self.MkArray.markers.pop(0)
+                self.MkArray.markers.append(msg)
+            self.position_pub.publish(self.MkArray)   
 
 
         
